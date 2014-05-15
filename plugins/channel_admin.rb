b2e8f5894a48f0bb9692a9b5571eec3efba95715
Marcelo Paez Sequeira <nkr@Casa-do-Capeta.local>
2014-05-15 15:22:59 -0300

    [ ! ] Started Channel Admin plugin

--- plugins/channel_admin.rb
+++ plugins/channel_admin.rb
@@ -3,13 +3,35 @@ class Cinch::ChannelAdmin
 
   @owners = ["nkr"]
 
-  def self.is_owner?(user)
+  match(/voice (.*)/,   method: :voice)
+  match(/devoice (.*)/, method: :devoice)
+  match(/op (.*)/,      method: :op)
+  match(/deop (.*)/,    method: :deop)
+
+  def is_owner?(user)
     return false unless user.authed?
     @owners.include? user.to_s
   end
 
-  def self.is_admin?(user)
+  def is_admin?(user)
     return false unless user.authed?
     @admins.include? user.to_s
   end
+
+  def voice(m, param)
+    m.channel.voice(param) if is_owner?(m.user)
+  end
+
+  def devoice(m, param)
+    m.channel.devoice(param) if is_owner?(m.user)
+  end
+
+  def op(m, param)
+    m.channel.op(param) if is_owner?(m.user)
+  end
+
+  def deop(m, param)
+    m.channel.deop(param) if is_owner?(m.user)
+  end
+
 end
