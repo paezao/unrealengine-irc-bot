05c83b0fbf2a6a2846965d43f0706f2df1d76a2d
Marcelo Paez Sequeira <paezao@gmail.com>
2015-02-04 15:57:25 -0200

    [ ! ] Added config

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'cinch'
 require 'cinch/toolbox'
 require 'cinch/cooldown'
 require 'httparty'
+require 'json'
 require './plugins/channel_admin'
 require './plugins/unrealdocs'
 require './plugins/ueschool'
@@ -9,10 +10,13 @@ require './plugins/officialstream'
 require './plugins/greenlight'
 require './plugins/streams'
 
+file = File.read('config.json')
+config = JSON.parse(file)
+
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = 'chat.freenode.org'
-    c.password = ENV['UE_IRC_PASSWORD']
+    c.password = config['password']
     c.channels = ['#unrealengine', '#netherdeep', '#ue4linux']
     c.nick = 'UnrealEngine'
     c.plugins.plugins = [ChannelAdmin, UnrealDocs, UESchool, OfficialStream, Greenlight, Streams]
