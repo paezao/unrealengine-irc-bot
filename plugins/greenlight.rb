9abc17492e11b728714444eed221063d1e2a91d6
Marcelo Paez Sequeira <paezao@gmail.com>
2015-06-22 14:32:13 -0300

    [ ! ] Added another game to greenlight

--- plugins/greenlight.rb
+++ plugins/greenlight.rb
@@ -5,7 +5,8 @@ class Greenlight
     { name: "Spellsworn", url: "http://steamcommunity.com/sharedfiles/filedetails/?id=385795837", by: "Omberone" },
     { name: "Exodus", url: "http://steamcommunity.com/sharedfiles/filedetails/?id=264273221", by: "Bob_Gneu & ReqPro" },
     { name: "Barrage", url: "http://steamcommunity.com//sharedfiles/filedetails/?id=414409258", by: "Daekesh" },
-    { name: "Vortex", url: "http://steamcommunity.com/sharedfiles/filedetails/?id=424753403", by: "k8nn" }
+    { name: "Vortex", url: "http://steamcommunity.com/sharedfiles/filedetails/?id=424753403", by: "k8nn" },
+    { name: "Descent: Underground", url: "https://steamcommunity.com/sharedfiles/filedetails/?id=412870542", by: "Stormwind" }
   ]
 
   match "greenlight"
